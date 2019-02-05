6fb65f63b93b8e61eba5b4f73a09e4d38a1601d1
Stephanie Jones <stephanie@Stephanies-MacBook-Pro.local>
2019-02-05 14:14:54 -0700

    added date template

--- dilf/src/App.js
+++ dilf/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Date from './Components/Date'
 import Image from './Components/Image'
 import Joke from './Components/Joke'
 
@@ -7,6 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="container">
+        <Date />
         <Image />
         <Joke />
       </div>
