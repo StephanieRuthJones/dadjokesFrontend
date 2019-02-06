b31b92a0dfc084d4c203f6b28fbd71e66cc5594b
Stephanie Jones <stephanie@Stephanies-MacBook-Pro.local>
2019-02-06 08:23:39 -0700

    Test commit

--- dilf/src/App.js
+++ dilf/src/App.js
@@ -13,38 +13,56 @@ class App extends Component {
   }
 
   async componentDidMount() {
-    const response = await fetch('http://localhost:3000')
+    const response = await fetch('http://localhost:8000')
     const json = await response.json()
     this.setState({ jokes: json })
     this.generateJoke()
   }
   //ternary for card vs loading bc loading delay for backend
+  formatDate = (date) => {
+    var monthNames = [
+      "January", "February", "March",
+      "April", "May", "June", "July",
+      "August", "September", "October",
+      "November", "December"
+    ];
 
-  getRandomIntInclusive = (max) => {
-    const min = 0
-    return Math.floor(Math.random() * (max - min)) + min
-  }
+    var day = date.getDate();
+    var monthIndex = date.getMonth();
+    var year = date.getFullYear();
 
-  generateJoke = () => {
-    console.log('gen joke clicked')
-    const randomIndex = this.getRandomIntInclusive(this.state.jokes.length)
-    const method = this.state.jokes[randomIndex]
-    console.log(method)
-    this.setState({
-      id: jokes.id,
-      joke: jokes.joke,
-    })
-  }
-  
-  render() {
-    return (
-      <div className="container">
-        <Date />
-        <Image />
-        <Joke />
-      </div>
-    );
-  }
+
+
+    this.setState({ date: { day + ' ' + monthNames[monthIndex] + ' ' + year } })
+}
+
+getRandomIntInclusive = (max) => {
+  const min = 0
+  return Math.floor(Math.random() * (max - min)) + min
+}
+
+generateJoke = () => {
+  const randomIndex = this.getRandomIntInclusive(this.state.jokes.length)
+  const joke = this.state.jokes[randomIndex]
+  console.log(joke)
+  this.setState({
+    id: joke.id,
+    joke: joke.text,
+    url: joke.url
+  })
+}
+
+render() {
+  return (
+    <div className="container">
+      <Date />
+      <Image
+        url={this.state.url} />
+      <Joke
+        joke={this.state.joke} />
+    </div>
+  );
+}
 }
 
 export default App;
--- dilf/src/Components/Date.js
+++ dilf/src/Components/Date.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const Date = () => {
+   
+
     return (
         <div className="container">
-            <p>Date</p>
+            <p>{formatDate()}</p>
         </div>
     )
 }
